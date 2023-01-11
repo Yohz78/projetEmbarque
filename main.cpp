bbaf3532304c37d00d25f3ba6d8ec8c9833e603c
starshooter <starshooter64@yahoo.fr>
2023-01-11 15:30:45 +0100

    conflict

--- main.cpp
+++ main.cpp
@@ -1,15 +1,9 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
+#include <iostream>
+
 #include "handler.h"
-
-
-int main() {
-=======
-#include <iostream>
-
 #include "src/bme/bme.h"
 #include "src/hmc/hmc.h"
 #include "src/hcsr/hcsr.h"
@@ -17,17 +11,6 @@ int main() {
 int main() {
 
     std::cout <<"toto" << std::endl;
->>>>>>> 7a1dd60728b945a6663a492292926f5051c227c0
-=======
-#include <iostream>
 
-#include "src/bme/bme.h"
-#include "src/hmc/hmc.h"
-#include "src/hcsr/hcsr.h"
-
-int main() {
-
-    std::cout <<"toto" << std::endl;
->>>>>>> 7a1dd60728b945a6663a492292926f5051c227c0
     return 0;
 }
