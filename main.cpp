86a8075479dd66e1e2f2d802484f879b0ec28626
root <root@Leo>
2023-01-11 11:29:47 +0100

    starting to work on main threading

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
 
-using namespace std;
+void *print_message(void *ptr) {
+    while (1) {
+        char *message;
+        message = (char *) ptr;
+        printf("%s\n", message);
+        sleep(1);
+    }
+}
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    pthread_t thread1;
+    const char *message1 = "Thread 1";
+    int  iret1;
+
+    iret1 = pthread_create(&thread1, NULL, print_message, (void*) message1);
+    pthread_join( thread1, NULL);
     return 0;
 }
