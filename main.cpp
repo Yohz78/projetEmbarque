62e962ac77949ed1b357c153c4f8a1d53b59285b
root <root@Leo>
2023-01-11 15:25:37 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+<<<<<<< HEAD
 #include "handler.h"
 
 
 int main() {
+=======
+#include <iostream>
+
+#include "src/bme/bme.h"
+#include "src/hmc/hmc.h"
+#include "src/hcsr/hcsr.h"
+
+int main() {
+
+    std::cout <<"toto" << std::endl;
+>>>>>>> 7a1dd60728b945a6663a492292926f5051c227c0
     return 0;
 }
