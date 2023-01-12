4c42fb48f76c865db6643bfd9acd812545271921
starshooter <starshooter64@yahoo.fr>
2023-01-12 12:50:09 +0100

    test for hmc / make 6

--- Balisa/src/handler/handler.cpp
+++ Balisa/src/handler/handler.cpp
@@ -11,6 +11,9 @@ Handler::~Handler()
 }
 
 void Handler::getHMCvalue(){
+    this->hmc.readX();
+    this->hmc.readY();
+    this->hmc.readZ();
     std::cout<<"x = "<<this->hmc.getX()<<std::endl;
     std::cout<<"y = "<<this->hmc.getY()<<std::endl;
     std::cout<<"z = "<<this->hmc.getZ()<<std::endl;
