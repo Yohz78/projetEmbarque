877ed40c7247969469a6705359e216f105753a82
starshooter <starshooter64@yahoo.fr>
2023-01-12 14:42:52 +0100

    Test handler as a whole 2

--- Balisa/main.cpp
+++ Balisa/main.cpp
@@ -6,7 +6,14 @@
 //#include "src/bme/bme.h"
 int main() {
 
+
     Handler handler;
+    while(1){
+    for(int i=0;i<10;i++){
+        sleep(1);
+        std::cout << "-" ;
+    }
+    std::endl;
     std::cout << "---------------------BME280---------------------" << std::endl;
     handler.getBME().harvestDataAndRun();
     std::cout << "---------------------HMC---------------------" << std::endl;
@@ -14,6 +21,6 @@ int main() {
     std::cout << "---------------------HCSR---------------------" << std::endl;
     std::cout << "Mouvement: " << handler.getHCSR().checkMotion() << std::endl;
     std::cout << "----------------------------------------------" << std::endl;
-
+    }
     return 0;
 }
