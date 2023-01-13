f345c1addd068770433ac3e4258d144553f6de87
root <root@Leo>
2023-01-13 12:09:33 +0100

    Fixing master's main

--- Master/main.cpp
+++ Master/main.cpp
@@ -23,7 +23,7 @@ void* read_sensor_data(void* args) {
 }
 
 int main() {
-    fd = serialOpen("/dev/ttyAMA0", 9600); // Ouvre le port série sur /dev/ttyAMA0 à 9600 bauds
+    fd = serialOpen("/dev/tty0", 9600); // Ouvre le port série sur /dev/ttyAMA0 à 9600 bauds
     pthread_create(&thread, NULL, read_sensor_data, NULL);
     pthread_join(thread, NULL);
     serialClose(fd); // Ferme le port série
