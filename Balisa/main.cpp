9a2747a8c7fef85bf512af0a7af0247c9eec5076
starshooter <starshooter64@yahoo.fr>
2023-01-16 12:11:17 +0100

    TX/RX cleaned main.cpp on master and slave, debug time

--- Balisa/main.cpp
+++ Balisa/main.cpp
@@ -1,4 +1,3 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
@@ -8,75 +7,6 @@
 
 #include "src/handler/handler.h"
 //#include "src/bme/bme.h"
-
-/**
- * @brief This function gets a command input from the Master and execute the command. 
- * 
- * @param[in] fd 
- * @param[in] Handler handler 
- */
-void loop(int fd,Handler* handler) {
-    if (serialDataAvail(fd)) {
-        char command[20];
-        int index=0;
-        while(serialGetchar(fd)!='\n'){
-            command[index] = serialGetchar(fd);
-            index++;
-        }
-        command[index]='\0';
-        if(strcmp(command,"BME")==0) {
-            std::cout << "---------------------BME280---------------------" << std::endl;
-            std::cout << handler->getBME().harvestDataAndRun() << std::endl;
-            serialPuts(fd,handler->getBME().harvestDataAndRun().c_str());
-        }
-        if(strcmp(command,"HMC")==0) {
-            std::cout << "---------------------HMC---------------------" << std::endl;
-            std::cout << "Valeur HMC : " << handler->getHMCvalue() << std::endl;
-            serialPuts(fd,handler->getHMCvalue().c_str()); // SENDING DATA
-        }
-        if(strcmp(command,"HCSR")==0) {
-            std::cout << "---------------------HCSR---------------------" << std::endl;
-            std::string data = "Mouvement: " + std::to_string(handler->getHCSR().checkMotion()) + "\n";
-            serialPuts(fd, data.c_str()); // SENDING DATA
-        }
-        if(strcmp(command,"all")==0) {
-            std::cout << "---------------------BME280---------------------" << std::endl;
-            std::cout << handler->getBME().harvestDataAndRun() << std::endl;
-            serialPuts(fd,handler->getBME().harvestDataAndRun().c_str());
-            std::cout << "---------------------HMC---------------------" << std::endl;
-            std::cout << "Valeur HMC : " << handler->getHMCvalue() << std::endl;
-            serialPuts(fd,handler->getHMCvalue().c_str()); // SENDING DATA
-            std::cout << "---------------------HCSR---------------------" << std::endl;
-            std::string data = "Mouvement: " + std::to_string(handler->getHCSR().checkMotion()) + "\n";
-            serialPuts(fd, data.c_str()); // SENDING DATA
-            std::cout << "----------------------------------------------" << std::endl;
-        }
-    }
-}
-
-void lecture(int fd){
-    std::string commande = "all";
-    std::cout << "On envoie la commande " + commande + " au slave" << std::endl;
-        serialPuts(fd,commande.c_str());
-        sleep(1);
-        if (serialDataAvail(fd) > 0) { // Vérifie s'il y a des données disponibles pour lecture
-            std::string data;
-            while (serialDataAvail(fd) > 0) {
-                char c = serialGetchar(fd);
-                data += c;
-            }
-            std::cout << "Données reçues : " << data << std::endl;
-        }else{
-            std::cout << "J'ai pas trouvé de données chef !!! " << std::endl;    
-    } 
-}
-
-void ecriture_rand(int fd){
-    int data = rand() % 100; // Génère un nombre aléatoire entre 0 et 99
-    std::string data_str = std::to_string(data); // Convertit le nombre en chaîne de caractères
-    serialPuts(fd, data_str.c_str()); // Envoie les données sur le port série */ 
-    std::cout << "Données envoyées : " << data_str << std::endl;    
-}
 
 int main() {
     int fd = serialOpen("/dev/ttyAMA0", 9600); // Ouvre le port série sur /dev/ttyS0 à 9600 bauds
@@ -91,12 +21,8 @@ int main() {
         std::cout << "Valeur de fd=====================> " << fd << std::endl;
         std::cout << "Valeur de serialDataAvail(fd)====> " << serialDataAvail(fd) << std::endl;
         
-        
-        //lecture();
-        std::string commande = "all";
-        std::cout << "On envoie la commande " + commande + " au slave" << std::endl;
-        serialPuts(fd,commande.c_str());
-        sleep(1);
+
+        //lecture
         if (serialDataAvail(fd) > 0) { // Vérifie s'il y a des données disponibles pour lecture
             std::string data;
             while (serialDataAvail(fd) > 0) {
@@ -108,96 +34,8 @@ int main() {
         else{
             std::cout << "J'ai pas trouvé de données chef !!! " << std::endl;    
         } 
-        
-        
-        sleep(1); // Fait une pause pendant 1 seconde
-        /*int data = rand() % 100; // Génère un nombre aléatoire entre 0 et 99
-        std::string data_str = std::to_string(data); // Convertit le nombre en chaîne de caractères
-        serialPuts(fd, data_str.c_str()); // Envoie les données sur le port série */ 
-        // std::cout << "Données envoyées : " << data_str << std::endl; 
-
-       /*if (serialDataAvail(fd)) {
-            char command[20];
-            int index=0;
-            while(serialGetchar(fd)!='\n'){
-                command[index] = serialGetchar(fd);
-                index++;
-            }
-            command[index]='\0';
-            /*if(strcmp(command,"BME")==0) {
-                std::cout << "---------------------BME280---------------------" << std::endl;
-                std::cout << handler.getBME().harvestDataAndRun() << std::endl;
-                serialPuts(fd,handler.getBME().harvestDataAndRun().c_str());
-            }
-            if(strcmp(command,"HMC")==0) {
-                std::cout << "---------------------HMC---------------------" << std::endl;
-                std::cout << "Valeur HMC : " << handler.getHMCvalue() << std::endl;
-                serialPuts(fd,handler.getHMCvalue().c_str()); // SENDING DATA
-            }
-            if(strcmp(command,"HCSR")==0) {
-                std::cout << "---------------------HCSR---------------------" << std::endl;
-                std::string data = "Mouvement: " + std::to_string(handler.getHCSR().checkMotion()) + "\n";
-                serialPuts(fd, data.c_str()); // SENDING DATA
-            }*/
-            /*if(strcmp(command,"all")==0) {
-                std::cout << "---------------------BME280---------------------" << std::endl;
-                std::cout << handler.getBME().harvestDataAndRun() << std::endl;
-                serialPuts(fd,handler.getBME().harvestDataAndRun().c_str());
-                std::cout << "---------------------HMC---------------------" << std::endl;
-                std::cout << "Valeur HMC : " << handler.getHMCvalue() << std::endl;
-                serialPuts(fd,handler.getHMCvalue().c_str()); // SENDING DATA
-                std::cout << "---------------------HCSR---------------------" << std::endl;
-                std::string data = "Mouvement: " + std::to_string(handler.getHCSR().checkMotion()) + "\n";
-                serialPuts(fd, data.c_str()); // SENDING DATA
-                std::cout << "----------------------------------------------" << std::endl;
-            }*/
-        
-        }
-
+        sleep(1); // Fait une pause pendant 1 seconde  
+    }
     serialClose(fd); // Ferme le port série
     return 0;
-    }
-
-
-
-
-/*
-int main() {
-
-
-    Handler handler;
-    int fd = serialOpen("/dev/ttyS0", 9600);
-    if (fd < 0) {
-        std::cout << "Error: Unable to open UART device" << std::endl;
-        return -1;
-    }
-        //loop(fd,&handler);   
-
-        serialPuts(fd,"TOTO");
-        serialPuts(fd,"TOTO1");
-        serialPuts(fd,"TOTO2");
-        serialPuts(fd,"TOTO3");
-        serialPuts(fd,"TOTO4");
-        serialPuts(fd,"TOTO5");
-        serialPuts(fd,"TOTO6");
-        serialPuts(fd,"TOTO7");
-        serialPuts(fd,"TOTO8");
-
-
-    return 0;
 }
-*/
-
-
-/*for(int i=0;i<10;i++){
-        delay(1);
-        std::cout << "-" ;
-    }
-    std::cout << std::endl;
-    std::cout << "---------------------BME280---------------------" << std::endl;
-    handler.getBME().harvestDataAndRun();
-    std::cout << "---------------------HMC---------------------" << std::endl;
-    handler.getHMCvalue();
-    std::cout << "---------------------HCSR---------------------" << std::endl;
-    std::cout << "Mouvement: " << handler.getHCSR().checkMotion() << std::endl;
-    std::cout << "----------------------------------------------" << std::endl;*/
--- Master/main.cpp
+++ Master/main.cpp
@@ -1,58 +1,6 @@
 #include <wiringSerial.h>
-#include <pthread.h>
 #include <unistd.h> // pour sleep
 #include <iostream>
-
-int fd;
-int interval = 1; // Intervalle en secondes
-pthread_t thread;
-
-void* read_sensor_data(void *args) {
-    while (true) {
-        char data[10000];
-        int index = 0;
-        serialPuts(fd, "all"); 
-        while (serialDataAvail(fd) > 0) {
-            int c = serialGetchar(fd);
-            if (c < 0) {
-                std::cout << "Error: Unable to receive data over UART" << std::endl;
-            }
-            std::cout << "Des données sont présentes : " << data << std::endl;
-            data[index] = c;
-            index++;
-        }
-        data[index] = '\0';
-        std::cout << "Données reçues : " << data << std::endl;
-        std::cout << "Données reçues : " << data[0] << std::endl;
-        sleep(interval); // Fait une pause pendant interval secondes
-    }
-}
-
-
-void lecture(int fd){
-    std::string commande = "all";
-    std::cout << "On envoie la commande " + commande + " au slave" << std::endl;
-    serialPuts(fd,commande.c_str());
-    sleep(1);
-    if (serialDataAvail(fd) > 0) { // Vérifie s'il y a des données disponibles pour lecture
-        std::string data;
-            while (serialDataAvail(fd) > 0) {
-                char c = serialGetchar(fd);
-                data += c;
-            }
-        std::cout << "Données reçues : " << data << std::endl;
-        }else{
-            std::cout << "J'ai pas trouvé de données chef !!! " << std::endl;    
-    } 
-}
-
-void ecriture_rand(){
-    int data = rand() % 100; // Génère un nombre aléatoire entre 0 et 99
-    std::string data_str = std::to_string(data); // Convertit le nombre en chaîne de caractères
-    serialPuts(fd, data_str.c_str()); // Envoie les données sur le port série */ 
-    std::cout << "Données envoyées : " << data_str << std::endl;    
-}
-
 
 int main() {
     int fd = serialOpen("/dev/ttyAMA0", 9600); // Ouvre le port série sur /dev/ttyAMA0 à 9600 bauds
@@ -68,7 +16,7 @@ int main() {
         std::cout << "Valeur de fd=====================> " << fd << std::endl;
         std::cout << "Valeur de serialDataAvail(fd)====> " << serialDataAvail(fd) << std::endl;
         
-        //ecriture_rand();
+        //ecriture_rand;
         int data = rand() % 100; // Génère un nombre aléatoire entre 0 et 99
         std::string data_str = std::to_string(data); // Convertit le nombre en chaîne de caractères
         serialPuts(fd, data_str.c_str()); // Envoie les données sur le port série */ 
@@ -82,16 +30,3 @@ int main() {
     return 0;
 }
 
-/*
-int main() {
-    fd = serialOpen("/dev/ttyAMA0", 9600); // Ouvre le port série sur /dev/ttyAMA0 à 9600 bauds
-    if (fd < 0) {
-        std::cout << "Error: Unable to open UART device" << std::endl;
-        return -1;
-    }
-    pthread_create(&thread, NULL, read_sensor_data, NULL);
-    pthread_join(thread, NULL);
-    serialClose(fd); // Ferme le port série
-    return 0;
-}
-*/
