78f2cac3ea0c80fa1a8ed4241ca719bb24ce3aab
starshooter <starshooter64@yahoo.fr>
2023-01-16 16:42:09 +0100

    test tx rx

--- Master/main.cpp
+++ Master/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 //#include <json/json.h>
 #include "src/pca/pca.h"
+#include <PCA9685.h>
 
 void* read_sensor_data(int fd) {
     while (true) {
@@ -23,29 +24,16 @@ void* read_sensor_data(int fd) {
 }
 
 int main() {
+
+    
     int fd = serialOpen("/dev/ttyAMA0", 9600); // Ouvre le port série sur /dev/ttyAMA0 à 9600 bauds
     if (fd < 0) {
         std::cout << "Error: Unable to open UART device" << std::endl;
         return -1;
     }
-
-    PCA9685 controle_servo;
-    std::cout << "controle_servo.init()" << std::endl;
-    controle_servo.init();
-    std::cout << "controle_servo.setServo(0,45)" << std::endl;
-    controle_servo.setServo(0,45);
-    sleep(3);
-    std::cout << "controle_servo.setServo(0,90)" << std::endl;
-    controle_servo.setServo(0,90);
-
-    std::cout << "controle_servo.setServo(1,45)" << std::endl;
-    controle_servo.setServo(1,45);
-    sleep(3);
-    std::cout << "controle_servo.setServo(1,90)" << std::endl;
-    controle_servo.setServo(1,90);
-    /*while (true) {
+    while (true) {
         read_sensor_data(fd);
-    }*/
+    }
 
     serialClose(fd); // Ferme le port série
     return 0;
