d4594454a2cdd01fd7380cfd8a3ab80dd4cd1787
starshooter <starshooter64@yahoo.fr>
2023-01-16 11:23:47 +0100

    some line swap

--- Balisa/main.cpp
+++ Balisa/main.cpp
@@ -15,33 +15,6 @@
  * @param[in] fd 
  * @param[in] Handler handler 
  */
-
-
-int main() {
-    int fd = serialOpen("/dev/ttyAMA0", 9600); // Ouvre le port série sur /dev/ttyS0 à 9600 bauds
-    if (fd < 0) {
-        std::cout << "Error: Unable to open UART device" << std::endl;
-        return -1;
-    }
-
-    Handler handler_test; // Creation du wrapper
-
-    while (true) {
-        /*std::cout << "Valeur de fd=====================> " << fd << std::endl;
-        int data = rand() % 100; // Génère un nombre aléatoire entre 0 et 99
-        std::string data_str = std::to_string(data); // Convertit le nombre en chaîne de caractères
-        serialPuts(fd, data_str.c_str()); // Envoie les données sur le port série */ 
-        // std::cout << "Données envoyées : " << data_str << std::endl; 
-
-        loop(fd,&handler_test); // Query / Answer Query
-        sleep(5); // Fait une pause pendant 1 seconde
-    }
-
-    serialClose(fd); // Ferme le port série
-    return 0;
-}
-
-
 void loop(int fd,Handler* handler) {
     if (serialDataAvail(fd)) {
         char command[20];
@@ -80,6 +53,33 @@ void loop(int fd,Handler* handler) {
         }
     }
 }
+
+int main() {
+    int fd = serialOpen("/dev/ttyAMA0", 9600); // Ouvre le port série sur /dev/ttyS0 à 9600 bauds
+    if (fd < 0) {
+        std::cout << "Error: Unable to open UART device" << std::endl;
+        return -1;
+    }
+
+    Handler handler_test; // Creation du wrapper
+
+    while (true) {
+        /*std::cout << "Valeur de fd=====================> " << fd << std::endl;
+        int data = rand() % 100; // Génère un nombre aléatoire entre 0 et 99
+        std::string data_str = std::to_string(data); // Convertit le nombre en chaîne de caractères
+        serialPuts(fd, data_str.c_str()); // Envoie les données sur le port série */ 
+        // std::cout << "Données envoyées : " << data_str << std::endl; 
+
+        loop(fd,&handler_test); // Query / Answer Query
+        sleep(5); // Fait une pause pendant 1 seconde
+    }
+
+    serialClose(fd); // Ferme le port série
+    return 0;
+}
+
+
+
 
 /*
 int main() {
