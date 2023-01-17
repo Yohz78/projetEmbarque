487d0f710d0f178f88743571fa8d1e87d528fa9b
starshooter <starshooter64@yahoo.fr>
2023-01-17 16:38:51 +0100

    working on create_db.cpp and import_json.cpp

---
+++ Server/main.cpp
@@ -0,0 +1,3 @@
+
+
+#define dbName "data.db"
--- Server/src/Create_db.cpp
+++ Server/src/Create_db.cpp
@@ -14,9 +14,25 @@ void createDatabase(const std::string &dbName) {
         return;
     }
 
-    // Create table
-    char *sql = "CREATE TABLE sensor_data (timestamp TEXT, temperature REAL, pressure REAL, humidity REAL, x INTEGER, y INTEGER, z INTEGER, mvt INTEGER)";
+    // Check if table exist and if not, create table
+    char *table_name = "sensor_data";
+    char *db_name = "main";
+    char *column_name = NULL;
+    rc = sqlite3_table_column_metadata(db, db_name, table_name, column_name, NULL, NULL, NULL, NULL, NULL);
+    if(rc == SQLITE_OK) {
+        printf("Table already exists\n");
+    }
+    else {
+    char *sql = "CREATE TABLE sensor_data (timestamp TEXT,~
+                                             temperature REAL,
+                                              pressure REAL,
+                                               humidity REAL,
+                                                x INTEGER,
+                                                 y INTEGER,
+                                                  z INTEGER,
+                                                   mvt INTEGER)";
     rc = sqlite3_exec(db, sql, NULL, 0, &zErrMsg);
+    }
 
     if( rc != SQLITE_OK ){
         fprintf(stderr, "SQL error: %s\n", zErrMsg);
