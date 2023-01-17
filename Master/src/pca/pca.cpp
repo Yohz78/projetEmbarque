164eaf22a12e5027b81b13a504f8a24245f3f3aa
starshooter <starshooter64@yahoo.fr>
2023-01-17 10:17:28 +0100

    changes to class pca, and new files i2c

--- Master/src/pca/pca.cpp
+++ Master/src/pca/pca.cpp
@@ -1,6 +1,9 @@
+#include <unistd.h>
+#include <math.h>
+
 #include "pca.h"
 
-void PCA9685::init(){
+/*void PCA9685::init(){
     fd = wiringPiI2CSetup(this->PCA9685_ADDRESS);
     wiringPiI2CWriteReg8(this->fd, this->PCA9685_MODE1, 0x00);
     int prescale = 25000000;
@@ -22,4 +25,115 @@ void PCA9685::setServo(int servo, int angle){
     wiringPiI2CWriteReg8(this->fd, LED0_ON_H + 4 * servo, on >> 8);
     wiringPiI2CWriteReg8(this->fd, LED0_OFF_L + 4 * servo, off & 0xFF);
     wiringPiI2CWriteReg8(this->fd, LED0_OFF_H + 4 * servo, off >> 8);    
+}*/
+
+PCA9685::PCA9685(uint8_t i2c_bus, uint8_t i2c_address) : i2c_bus(i2c_bus), i2c_address(i2c_address), nom("PCA9685"), deviceI2C(nullptr)
+{
+    
+}
+
+PCA9685::~PCA9685(){
+    this->clean();
+}
+
+bool PCA9685::init(){
+    PLOGD << "Initialisation de la carte de controle des moteurs";
+    deviceI2C = new i2c(this->getAdresse(), this->getBus());
+    if (!deviceI2C->getError()){
+
+        deviceI2C->WriteReg8(MODE2, OUTDRV);
+        deviceI2C->WriteReg8(MODE1, ALLCALL);
+        //time.sleep(0.005)  # wait for oscillator
+        usleep(5000);
+        uint8_t mode1 = (uint8_t)deviceI2C->ReadReg8(MODE1);
+        mode1 = mode1 & ~SLEEP;  // wake up (reset sleep)
+        deviceI2C->WriteReg8(MODE1, mode1);
+        //time.sleep(0.005)  # wait for oscillator
+        usleep(5000);
+        setPwmFrequency(50);
+
+        error = false;
+        
+    }
+    else{
+	    error = true;
+    }
+    log_init(this->getNom(), !error);
+
+    return !error;
+}
+
+void PCA9685::clean()
+{
+    if (deviceI2C != NULL)
+    {
+        PLOGD << "Clean de " << this->getNom();
+        delete deviceI2C;
+        deviceI2C = nullptr;
+    }
+}
+
+void PCA9685::setPwmFrequency(uint16_t frequency){
+    float prescaleval = 25000000.0; //25MHz
+    prescaleval /= 4096.0;         //12-bit
+    prescaleval /= float(frequency);
+    prescaleval -= 1.0;
+    PLOGI <<"Setting PWM frequency to "<<frequency<<" hz";
+    PLOGI <<"Estimated pre-scale: "<<prescaleval;
+    int prescale = (int)floor(prescaleval + 0.5);
+    PLOGI <<"Final pre-scale: "<<prescale;
+    uint8_t oldmode = (uint8_t)deviceI2C->ReadReg8(MODE1);
+    uint8_t newmode = (oldmode & 0x7F) | 0x10; // sleep
+    deviceI2C->WriteReg8(MODE1, newmode);      //go to sleep
+    deviceI2C->WriteReg8(PRESCALE, prescale);
+    deviceI2C->WriteReg8(MODE1, oldmode);
+    //time.sleep(0.005)  # wait
+    usleep(5000);
+    deviceI2C->WriteReg8(MODE1, oldmode | 0x80);
+}
+
+void PCA9685::setPwm(uint8_t channel, uint16_t on, uint16_t off){
+    deviceI2C->WriteReg8(LED0_ON_L+4*channel, on & 0xFF);
+    deviceI2C->WriteReg8(LED0_ON_H+4*channel, on >> 8);
+    deviceI2C->WriteReg8(LED0_OFF_L+4*channel, off & 0xFF);
+    deviceI2C->WriteReg8(LED0_OFF_H+4*channel, off >> 8);
+}
+
+void PCA9685::setAllPwm(uint16_t on, uint16_t off){
+    deviceI2C->WriteReg8(ALL_LED_ON_L, on & 0xFF);
+    deviceI2C->WriteReg8(ALL_LED_ON_H, on >> 8);
+    deviceI2C->WriteReg8(ALL_LED_OFF_L, off & 0xFF);
+    deviceI2C->WriteReg8(ALL_LED_OFF_H, off >> 8);
+}
+
+void PCA9685::move(uint8_t channel, int deg){
+    float pwm = 570.0 + ((deg/180.0) * 1700.0);
+    pwm = (4096.0/20000.0) * pwm;
+    setPwm(channel, 0, (int) pwm);
+}
+
+void PCA9685::moveYellowFlag(int deg)
+{
+    move(yellowFlagChannel, deg);
+}
+
+void PCA9685::moveBlueFlag(int deg)
+{
+    move(blueFlagChannel, deg);
+}
+
+
+bool PCA9685::getError()
+{
+	return error;
+}
+
+uint8_t PCA9685::getAdresse()
+{
+    return i2c_address;
+}
+
+uint8_t PCA9685::getBus()
+{
+    return i2c_bus;
 }
--- Master/src/pca/pca.h
+++ Master/src/pca/pca.h
@@ -1,5 +1,82 @@
-#include <wiringPiI2C.h>
-#define LED0_ON_L 0x06
+#ifndef PCA_H
+#define PCA_H
+
+#include "../i2c/i2c.h"
+
+#define MODE1			0x00
+#define MODE2			0x01
+#define SUBADR1			0x02
+#define SUBADR2			0x03
+#define SUBADR3			0x04
+#define PRESCALE		0xFE
+#define LED0_ON_L		0x06
+#define LED0_ON_H		0x07
+#define LED0_OFF_L		0x08
+#define LED0_OFF_H		0x09
+#define ALL_LED_ON_L    0xFA
+#define ALL_LED_ON_H	0xFB
+#define ALL_LED_OFF_L	0xFC
+#define ALL_LED_OFF_H	0xFD
+// Bits
+#define RESTART			0x80
+#define SLEEP			0x10
+#define ALLCALL			0x01
+#define INVRT			0x10
+#define OUTDRV			0x04
+
+#define PAN			    0
+#define TILT			1
+#define FREQUENCY		50
+#define CLOCKFREQ		25000000
+#define PANOFFSET		1
+#define PANSCALE		1.4
+#define TILTOFFSET		30
+#define TILTSCALE		1.43
+#define PANMAX			270
+#define PANMIN			90
+#define TILTMAX			90
+#define TILTMIN			-45
+
+class PCA9685
+{
+public:
+    explicit PCA9685(uint8_t i2c_bus, uint8_t i2c_address);
+    ~PCA9685();
+    bool init();
+    void clean();
+    bool getError();
+    uint8_t getBus();
+    uint8_t getAdresse();
+    const char * getNom() {return nom;};
+
+    void moveYellowFlag(int deg);
+    void moveBlueFlag(int deg);
+
+private :
+    void setPwmFrequency(uint16_t frequency);
+    void setPwm(uint8_t channel, uint16_t on, uint16_t off);
+    void setAllPwm(uint16_t on, uint16_t off);
+    void move(uint8_t channel , int deg);
+
+
+
+    const uint8_t i2c_bus;
+    const uint8_t i2c_address;
+    const char* nom;
+
+    const uint8_t yellowFlagChannel =1;
+    const uint8_t blueFlagChannel = 2;
+
+
+    i2c *deviceI2C;
+
+    bool error;
+};
+
+#endif // PCA_H
+
+
+/*#define LED0_ON_L 0x06
 #define LED0_ON_H 0x07
 #define LED0_OFF_L 0x08
 #define LED0_OFF_H 0x09
@@ -17,4 +94,4 @@ class PCA9685 {
         void init();
 
         void setServo(int servo, int angle);
-};
+}; */ 
