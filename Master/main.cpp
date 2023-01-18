d20d7bdf7c1fefc35845d76747e39b9fa887c42d
starshooter <starshooter64@yahoo.fr>
2023-01-18 15:23:05 +0100

    remove pthread from  main for now

--- Master/main.cpp
+++ Master/main.cpp
@@ -147,15 +147,15 @@ int main() {
         return -1;
     }
     vector<Json::Value> res;
-    pthread_t thread_rw, thread_send;
+    /*pthread_t thread_rw, thread_send;
 
     pthread_create(&thread_rw, NULL, read_and_write, &fd, &res);
     pthread_create(&thread_send, NULL,send_boost, &fd, &res);
     pthread_join(thread_rw, NULL);
-    pthread_join(thread_send, NULL);
+    pthread_join(thread_send, NULL);*/ 
 
     // C  est ici que l'on va avoir besoin des threads, un qui recupere de la data et un qui envoie, les deux a differents intervalles
-    read_and_write(fd, res);
+    //read_and_write(fd, res);
 
     serialClose(fd); // Ferme le port série
     return 0;
