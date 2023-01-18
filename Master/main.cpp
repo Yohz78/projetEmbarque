2c62d98fb04874a714f5bf7a8f034ecec09108f6
root <root@Leo>
2023-01-18 15:20:22 +0100

    Fixing master's main.cpp

--- Master/main.cpp
+++ Master/main.cpp
@@ -155,7 +155,7 @@ int main() {
     pthread_join(thread_send, NULL);
 
     // C  est ici que l'on va avoir besoin des threads, un qui recupere de la data et un qui envoie, les deux a differents intervalles
-    read_and_write(fd, &res);
+    read_and_write(fd, res);
 
     serialClose(fd); // Ferme le port série
     return 0;
