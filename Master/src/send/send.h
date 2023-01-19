2092fe05f891812d7bc69b7e032643c1bdc4d569
starshooter <starshooter64@yahoo.fr>
2023-01-19 11:49:42 +0100

    multiple def of funcs ??? 4

--- Master/src/send/send.h
+++ Master/src/send/send.h
@@ -1,3 +1,6 @@
+#ifndef SEND_H
+#define SEND_H
+
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
@@ -24,3 +27,4 @@ std::string read_sensor_data(int);
 void read_and_write(int, vector<Json::Value>&);
 void sendData(vector<Json::Value>&);
 
+#endif //SEND_H
