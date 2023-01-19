3c3434125c89b4ec96c2bcfa66723dc4ecc3abfe
starshooter <starshooter64@yahoo.fr>
2023-01-19 12:10:24 +0100

    working on server reception

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+
+
+#include <jsoncpp/json/json.h>
 #include "menu.cpp"
+#include "src/retrieve.h"
+
+using namespace std;
 
 int main(){
-    menu();
+    //menu();
+    vector<Json::Value> res = retrieve();
+    for(auto data: res){
+        cout << res;
+    }
+
+    return 0;
 }
 
