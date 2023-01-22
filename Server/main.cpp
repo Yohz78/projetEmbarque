2d3ac870fb7597ac9866ac2f8bc3773223cfc0c1
root <root@Leo>
2023-01-22 14:56:09 +0100

    Setting up menu for data.json exploitation

--- Server/main.cpp
+++ Server/main.cpp
@@ -20,3 +20,37 @@ int main(){
     return 0;
 }
 
+// #include <fstream>
+// #include <iostream>
+// #include <nlohmann/json.hpp>
+
+// using json = nlohmann::json;
+
+// int main() {
+//     // lire les données à partir du fichier JSON
+//     std::ifstream file("data.json");
+//     json root;
+//     file >> root;
+
+//     // itérer sur les données
+//     for (auto& measurement : root) {
+//         std::cout << "Date de la mesure : " << measurement["date"].get<std::string>() << std::endl;
+//         std::cout << "Données du capteur BME : " << std::endl;
+//         std::cout << "Température : " << measurement["BME"]["temperature"].get<std::string>() << std::endl;
+//         std::cout << "Pression : " << measurement["BME"]["pression"].get<std::string>() << std::endl;
+//         std::cout << "Humidité : " << measurement["BME"]["humidity"].get<std::string>() << std::endl;
+
+//         double presence = measurement["HCSR"]["mvt"].get<double>();
+//         if(presence==1){
+//             std::cout << "Capteur HCSR : " << "Présence détectée"<< std::endl;
+//         }else{
+//             std::cout << "Capteur HCSR : " << "Pas de présence détectée"<< std::endl;
+//         }
+
+//         std::cout << "Données du capteur HMC : " << std::endl;
+//         std::cout << "Coordonnée X : " << measurement["HMC"]["x"].get<std::string>() << std::endl;
+//         std::cout << "Coordonnée Y : " << measurement["HMC"]["y"].get<std::string>() << std::endl;
+//         std::cout << "Coordonnée Z : " << measurement["HMC"]["z"].get<std::string>() << std::endl;
+//     }
+//     return 0;
+// }
--- Server/src/menu/menu.cpp
+++ Server/src/menu/menu.cpp
@@ -1,70 +1,78 @@
 #include <iostream>
 #include <jsoncpp/json/json.h>
+#include <nlohmann/json.hpp>
+#include <fstream>
 
 #include "menu.h"
-using namespace std;
+using json = nlohmann::json;
 
-// std::istringstream json_stream(jsonString);
-// Json::Value root;
-// Json::CharReaderBuilder builder;
-// JSONCPP_STRING errs;
-// bool parsingSuccessful = Json::parseFromStream(builder,
-//                                                json_stream,
-//                                                &root,
-//                                                &errs);
+void afficheData(){
+    
+    std::ifstream file("data.json");
+    json root;
+    file >> root;
+    for (auto& measurement : root) {
+        std::cout << "Date de la mesure : " << measurement["date"].get<std::string>() << std::endl;
+        std::cout << "Données du capteur BME : " << std::endl;
+        std::cout << "Température : " << measurement["BME"]["temperature"].get<std::string>() << std::endl;
+        std::cout << "Pression : " << measurement["BME"]["pression"].get<std::string>() << std::endl;
+        std::cout << "Humidité : " << measurement["BME"]["humidity"].get<std::string>() << std::endl;
 
-void afficheData(Json::Value root){
-    std::cout << "Date de la mesure : " << root["date"].asString() << std::endl;
-    std::cout << "Données du capteur BME : " << std::endl;
-    std::cout << "Température : " << root["BME"]["temperature"].asString() << std::endl;
-    std::cout << "Pression : " << root["BME"]["pression"].asString() << std::endl;
-    std::cout << "Humidité : " << root["BME"]["humidity"].asString() << std::endl;
-    double presence = root["mvt"].asDouble();
-    if(presence==1){
-        std::cout << "Capteur HCSR : " << "Présence détectée"<< std::endl;
-    }else{
-        std::cout << "Capteur HCSR : " << "Pas de présence détectée"<< std::endl;
-    }
-    std::cout << "Données du capteur HMC : " << std::endl;
-    std::cout << "Coordonnée X : " << root["HMC"]["x"].asString() << std::endl;
-    std::cout << "Coordonnée Y : " << root["HMC"]["y"].asString() << std::endl;
-    std::cout << "Coordonnée Z : " << root["HMC"]["z"].asString() << std::endl;
+        double presence = measurement["HCSR"]["mvt"].get<double>();
+        if(presence==1){
+            std::cout << "Capteur HCSR : " << "Présence détectée"<< std::endl;
+        }else{
+            std::cout << "Capteur HCSR : " << "Pas de présence détectée"<< std::endl;
+        }
+
+        std::cout << "Données du capteur HMC : " << std::endl;
+        std::cout << "Coordonnée X : " << measurement["HMC"]["x"].get<std::string>() << std::endl;
+        std::cout << "Coordonnée Y : " << measurement["HMC"]["y"].get<std::string>() << std::endl;
+        std::cout << "Coordonnée Z : " << measurement["HMC"]["z"].get<std::string>() << std::endl;
+}
 }
 
 void afficheBME(Json::Value root){
-    std::cout << "Date de la mesure : " << root["date"].asString() << std::endl;
-    std::cout << "Données du capteur BME : " << std::endl;
-    std::cout << "Température : " << root["BME"]["temperature"].asString() << std::endl;
-    std::cout << "Pression : " << root["BME"]["pression"].asString() << std::endl;
-    std::cout << "Humidité : " << root["BME"]["humidity"].asString() << std::endl;    
+    std::ifstream file("data.json");
+    json root;
+    file >> root;
+    for (auto& measurement : root) {
+        std::cout << "Date de la mesure : " << measurement["date"].get<std::string>() << std::endl;
+        std::cout << "Données du capteur BME : " << std::endl;
+        std::cout << "Température : " << measurement["BME"]["temperature"].get<std::string>() << std::endl;
+        std::cout << "Pression : " << measurement["BME"]["pression"].get<std::string>() << std::endl;
+        std::cout << "Humidité : " << measurement["BME"]["humidity"].get<std::string>() << std::endl;
+    }
+       
 }
 
 void afficheHCSR(Json::Value root){
-    std::cout << "Date de la mesure : " << root["date"].asString() << std::endl;
-    double presence = root["mvt"].asDouble();
-    if(presence==1){
-        std::cout << "Capteur HCSR : " << "Présence détectée"<< std::endl;
-    }else{
-        std::cout << "Capteur HCSR : " << "Pas de présence détectée"<< std::endl;
-    }  
+    std::ifstream file("data.json");
+    json root;
+    file >> root;
+    for (auto& measurement : root) {
+        double presence = measurement["HCSR"]["mvt"].get<double>();
+        if(presence==1){
+            std::cout << "Capteur HCSR : " << "Présence détectée"<< std::endl;
+        }else{
+            std::cout << "Capteur HCSR : " << "Pas de présence détectée"<< std::endl;
+        }
+    }
 }
 
 void afficheHMC(Json::Value root){
-    std::cout << "Date de la mesure : " << root["date"].asString() << std::endl;
-    std::cout << "Données du capteur HMC : " << std::endl;
-    std::cout << "Coordonnée X : " << root["HMC"]["x"].asString() << std::endl;
-    std::cout << "Coordonnée Y : " << root["HMC"]["y"].asString() << std::endl;
-    std::cout << "Coordonnée Z : " << root["HMC"]["z"].asString() << std::endl;   
+    std::ifstream file("data.json");
+    json root;
+    file >> root;
+    for (auto& measurement : root) {
+        std::cout << "Données du capteur HMC : " << std::endl;
+        std::cout << "Coordonnée X : " << measurement["HMC"]["x"].get<std::string>() << std::endl;
+        std::cout << "Coordonnée Y : " << measurement["HMC"]["y"].get<std::string>() << std::endl;
+        std::cout << "Coordonnée Z : " << measurement["HMC"]["z"].get<std::string>() << std::endl;
+    }  
 }
 
 void menu(){
-    std::string jsonString= "{\"date:\" \"2023-01-17T15:38:16\",\"BME\": {\"temperature\": \"22.250000\",\"pression\": \"977.488647\",\"humidity\": \"29.601562\"},\"HCSR\": {\"mvt\": \"0\"},\"HMC\": {\"x\": \"18146\"\"y\": \"-358\",\"z\": \"-1587\"}}";
-    Json::Value root;
-    Json::Reader reader;
-    bool parsingSuccessful = reader.parse(jsonString, root);
-    if(!parsingSuccessful){
-        std::cout << "Error parsing JSON" << std::endl;
-    }else{
         cout << "Bonjour utilisateur!" << endl;
         while (true) {
             cout << "Veuillez choisir une option:" << endl;
@@ -99,5 +107,3 @@ void menu(){
         }
     }
     }
-}
-
